c463744f8fe3b0fafc80408e153fa585bc4a0e6d
gebre-a <kidanalemayehu12@gmail.com>
2024-02-24 03:17:24 -0800

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,8 +1,20 @@
-# Puppet file that set ssh config
-# # Password Authentication to None
-# # Connects with ~/.ssh/school private key
-#
-# file { '/home/user/.ssh/config':
-#         ensure  => present,
-#         content => "Host server\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
-#         }
+# using puppet to make changes to the default ssh config file
+# so that one can connect to a server without typing a password.
+
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
+}
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
+}
+
